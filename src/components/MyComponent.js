57c29a256001862044f6e15f034f1e556b1e7526
Emanuel Castillo <ecastillo76@misena.edu.co>
2024-01-01 21:14:36 +0100

    First commit

---
+++ src/components/MyComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyComponent = () => {
+    return (
+        <div className='bg-blue-500 text-white p-4'>
+            ¡Hola, este es mi componente con estilo TailWind
+        </div>
+    )
+}
+
+export default MyComponent;
