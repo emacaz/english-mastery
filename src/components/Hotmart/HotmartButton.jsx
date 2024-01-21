1cac0ed8a23548a7fbbfcd10823a4cb5b262f274
Emanuel Castillo <ecastillo76@misena.edu.co>
2024-01-22 00:05:56 +0100

    Modal window of buttons is not working on phones

--- src/components/Hotmart/HotmartButton.jsx
+++ src/components/Hotmart/HotmartButton.jsx
@@ -1,41 +1,41 @@
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
 
 const HotmartButton = ({ link }) => {
-  useEffect(() => {
-    const loadHotmart = () => {
-      // Check if the script is already loaded
-      if (
-        !document.querySelector(
-          'script[src="https://static.hotmart.com/checkout/widget.min.js"]'
-        )
-      ) {
-        var imported = document.createElement("script");
-        imported.src = "https://static.hotmart.com/checkout/widget.min.js";
-        document.head.appendChild(imported);
-      }
+  // useEffect(() => {
+  //   const loadHotmart = () => {
+  //     // Check if the script is already loaded
+  //     if (
+  //       !document.querySelector(
+  //         'script[src="https://static.hotmart.com/checkout/widget.min.js"]'
+  //       )
+  //     ) {
+  //       var imported = document.createElement("script");
+  //       imported.src = "https://static.hotmart.com/checkout/widget.min.js";
+  //       document.head.appendChild(imported);
+  //     }
 
-      // Check if the CSS is already loaded
-      if (
-        !document.querySelector(
-          'link[href="https://static.hotmart.com/css/hotmart-fb.min.css"]'
-        )
-      ) {
-        var link = document.createElement("link");
-        link.href = "https://static.hotmart.com/css/hotmart-fb.min.css";
-        document.head.appendChild(link);
-      }
-    };
-    loadHotmart();
-  }, []);
+  //     // Check if the CSS is already loaded
+  //     if (
+  //       !document.querySelector(
+  //         'link[href="https://static.hotmart.com/css/hotmart-fb.min.css"]'
+  //       )
+  //     ) {
+  //       var link = document.createElement("link");
+  //       link.href = "https://static.hotmart.com/css/hotmart-fb.min.css";
+  //       document.head.appendChild(link);
+  //     }
+  //   };
+  //   loadHotmart();
+  // }, []);
 
-  const buyProduct = (e) => {
-    e.preventDefault();
-  }
+  // const buyProduct = (e) => {
+  //   e.preventDefault();
+  // }
 
   return (
     <a
       className="hotmart-fb block text-center w-full text-base sm:max-w-[400px] bg-[#545e67] my-4 text-white p-2 rounded-lg sm:text-lg hover:bg-slate-800"
-      onClick={buyProduct}
+      // onClick={buyProduct}
       href={link}
     >
       Seleccionar
