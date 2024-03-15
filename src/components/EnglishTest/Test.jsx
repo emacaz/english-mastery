3a8857ae3ccd3b80d5668f2a3f2087daa881113a
Emanuel Castillo <ecastillo76@misena.edu.co>
2024-03-15 09:40:12 +0100

    Adding comments about how the test will be implemented

--- src/components/EnglishTest/Test.jsx
+++ src/components/EnglishTest/Test.jsx
@@ -28,10 +28,10 @@ const Test = () => {
 
   return (
     <section>
-      <h1>Bienvenido al Test de Inglés</h1>
+      <h1>Test de Inglés</h1>
       <p>
         Una vez empieces el test no debes recargar o actualizar la página,
-        tienes 20 minutos para completarlo y necesitas aprobar el 90% de
+        tienes 20 minutos para completarlo (aparecerá un temporizador) y necesitas aprobar el 90% de
         respuestas por nivel para aprobar dicho nivel. Cuando lo empieces no
         puedes volver atrás hasta terminarlo y una vez el tiempo se termine, se
         cierra el test. Good luck!
@@ -55,3 +55,23 @@ const Test = () => {
 };
 
 export default Test;
+
+/**
+ * press "Empezar Test"***:
+ * Desaparece todo lo anterior y aparece:
+ * [Timer, Nivel, Pregunta, Opciones de respuestas] - Componente para (nivel, pregunta, opciones de respuestas)
+ * 
+ * Al presionar una opción de respuesta...***:
+ * Guardar la respuesta y hacer su respectiva lógica,
+ * y a la siguiente... hasta terminar ó hasta que el timer se acabe
+ * 
+ * (En medio...): si el timer acaba o si han terminado las preguntas...***:
+ * MOSTRAR:
+ * Has terminado el test + puntuación
+ * Porcentaje de respuestas correctas por nivel:
+ * MOSTRAR respuestas por nivel***
+ * Nivel de Inglés: A,B,C...
+ * Botones: Ir a la página de Inicio - Tomar test nuevamente...
+ * 
+ * Botón: Tomar test nuevamente -> Dirige a *press "Empezar Test"***:
+ */
